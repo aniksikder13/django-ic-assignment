5725e3dce9293a0c6093c583056d52de450165c5
Anik Sikder <aniksikder9501@gmail.com>
2026-02-15 22:19:34 +0600

    Inserted test data in sqlite

--- library_management_app/admin.py
+++ library_management_app/admin.py
@@ -3,5 +3,13 @@ from .models import Author, Book, Category
 
 # Register your models here.
 admin.site.register(Author)
-admin.site.register(Book)
 admin.site.register(Category)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'status')
+    list_editable = ('status', )
+    list_filter = ('status', 'published_date', 'author', 'category')
+    list_per_page = 10
+
+
+admin.site.register(Book, BookAdmin)
